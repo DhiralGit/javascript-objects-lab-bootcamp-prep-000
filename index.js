a22bc609268e281a54bf03855af88f4dd446ad0d
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-20 19:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
