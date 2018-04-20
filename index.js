f85c60c31f96b0006683de540de2ea7b39f87bce
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-20 19:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = new Object({recipes: 'test'});
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
